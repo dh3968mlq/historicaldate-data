4c50510bcbcd269b564dc76de1a2c3b45746dd61
David Harris <dharris3968@hotmail.co.uk>
2023-07-11 16:15:54 +0100

    Add study range to unit tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 import sys
 import glob
 import pandas as pd
+import datetime
 
 hdroot = "/svol1/pishare/users/pi/repos/historicaldate"
 sys.path.append(hdroot)
@@ -9,14 +10,16 @@ from historicaldate import hdpl
 def test1():
     lfiles = glob.glob("data/**/*.csv", recursive=True)
     for filename in lfiles:
-        lowfolder = filename.split("/")[-2]
-        if lowfolder != "reference_tables":
-            df = pd.read_csv(filename, na_filter=False)
-            pltl = hdpl.plTimeLine()
-            try:
-                pltl.add_event_set(df, title=filename)
-            except Exception as inst:
-                print(f"Failed on {filename}")
-                raise
+        if "Playwrights" in filename or True:  # Allow easy focus on a problematic file
+            lowfolder = filename.split("/")[-2]
+            if lowfolder != "reference_tables":
+                df = pd.read_csv(filename, na_filter=False)
+                pltl = hdpl.plTimeLine()
+                try:
+                    pltl.add_event_set(df, title=filename,
+                                    study_range_start=datetime.date(1000,1,1), study_range_end=datetime.date(2100,12,31))
+                except Exception as inst:
+                    print(f"Failed on {filename}")
+                    raise
 
     assert True
